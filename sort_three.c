c3f36f16fbd69fe77939d427da24e430baa5f64c
Oshintha Sandaruwan <155742370+Oshintha@users.noreply.github.com>
2024-09-06 16:53:30 +0300

    Create sort_three.c

---
+++ sort_three.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 16:25:23 by aoshinth          #+#    #+#             */
+/*   Updated: 2024/09/06 16:34:51 by aoshinth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    sort_three(t_stack_node **a)
+{
+    t_stack_node    *biggest_node;
+
+    biggest_node = find_max(*a);
+    if (biggest_node == *a)
+        ra(a, false);
+    else if ((*a)->next == biggest_node)
+        rra(a, false);
+    if ((*a)->nbr > (*a)->next->nbr)
+        sa(a, false);
+}
