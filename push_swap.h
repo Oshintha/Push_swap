92edff3338dc96e9349ce6eb7ec6584ab17fe64e
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c2r4p5.hive.fi>
2024-11-13 11:44:43 +0200

    add modified file

--- push_swap.h
+++ push_swap.h
@@ -6,12 +6,13 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 10:22:35 by aoshinth          #+#    #+#             */
-/*   Updated: 2024/11/12 19:50:52 by aoshinth         ###   ########.fr       */
+/*   Updated: 2024/11/13 09:54:37 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
 # include <stdlib.h>
 # include <limits.h>
 # include <stdbool.h>
@@ -31,17 +32,17 @@ typedef struct s_stack_node
 	struct s_stack_node	*prev;
 }t_stack_node;	
 
-//***Handle errors
+//Handle errors
 int				error_syntax(char *str_n);
 int				error_duplicate(t_stack_node *a, int n);
 void			free_stack(t_stack_node **stack);
 void			free_errors(t_stack_node **a);
 
-//***Stack initiation
+//Stack initiation
 void			init_stack_a(t_stack_node **a, char **av);
 char			**split(char *s, char c);
 
-//***Nodes initiation
+//Nodes initiation
 void			init_nodes_a(t_stack_node *a, t_stack_node *b);
 void			init_nodes_b(t_stack_node *a, t_stack_node *b);
 void			current_index(t_stack_node *stack);
@@ -49,14 +50,14 @@ void			set_cheapest(t_stack_node *stack);
 t_stack_node	*get_cheapest(t_stack_node *stack);
 void			prep_for_push(t_stack_node **s, t_stack_node *n, char c);
 
-//***Stack utils
+//Stack utils
 int				stack_len(t_stack_node *stack);
 t_stack_node	*find_last(t_stack_node *stack);
 bool			stack_sorted(t_stack_node *stack);
 t_stack_node	*find_min(t_stack_node *stack);
 t_stack_node	*find_max(t_stack_node *stack);
 
-//***Commands
+//Commands
 void			sa(t_stack_node **a, bool print);
 void			sb(t_stack_node **b, bool print);
 void			ss(t_stack_node **a, t_stack_node **b, bool print);
@@ -69,7 +70,7 @@ void			rrr(t_stack_node **a, t_stack_node **b, bool print);
 void			pa(t_stack_node **a, t_stack_node **b, bool print);
 void			pb(t_stack_node **b, t_stack_node **a, bool print);
 
-//***Algorithm
+//Algorithm
 void			sort_three(t_stack_node **a);
 void			sort_stacks(t_stack_node **a, t_stack_node **b);
 void			rotate_both(t_stack_node **a, t_stack_node **b,
